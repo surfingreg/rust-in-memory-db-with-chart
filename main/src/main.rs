93aa8a8ef3fa331589ef31311efa7fb618293079
gp <github@swimr.com>
2024-01-17 13:45:36 -0800

    run alpaca and coinbase data feeds at the same time

--- main/src/main.rs
+++ main/src/main.rs
@@ -3,6 +3,7 @@
 use std::error::Error;
 use std::time::Duration;
 use chrono::{DateTime, Utc};
+use crossbeam_channel::Sender;
 use tokio::sync::oneshot;
 use common_lib::{ChartDataset, UniversalError, DbMsg, ProductId};
 use common_lib::cb_ticker::TickerSource;
@@ -10,6 +11,7 @@ use common_lib::init::init;
 use db::arrow_db;
 use visual::http_server;
 use ws::connect::ConnectSource;
+use ws_broadcast::command::Cmd;
 
 fn main() {
 
@@ -28,15 +30,12 @@ fn main() {
     // database thread
     let tx_db = arrow_db::run(tokio_runtime.handle().clone());
 
+    // run coinbase and alpaca threads
     let mut handles = vec!();
-
-    // coinbase websocket thread
     handles.push(ws::connect::run(ConnectSource::Coinbase, tx_db.clone()));
-
-    // alpaca websocket thread
     handles.push(ws::connect::run(ConnectSource::Alpaca, tx_db.clone()));
 
-    // outbound websocket
+    // broadcast websocket
     let (server_tx, server_rx) = crossbeam_channel::unbounded::<ws_broadcast::command::Cmd>();
     let _h1 = std::thread::spawn(move || {
         std::thread::spawn(|| {
@@ -45,46 +44,14 @@ fn main() {
         });
     });
 
-
+    // tokio runtime herein
     let tx_db2 = tx_db.clone();
     tokio_runtime.block_on(async {
-
+        // start 1-minute chart poll for websocket
         let tx_db3 = tx_db2.clone();
-        tokio::spawn(async move {
-            // todo: outbound websocket test; move somewhere else
-            // todo: get the date of the last chart data sent and send
+        spawn_chart_refresher(tx_db3, server_tx);
 
-
-            // a long, long time ago...
-            let most_recent_date: DateTime<Utc> = DateTime::<Utc>::from(DateTime::parse_from_rfc3339("2023-12-24T04:08:00-00:00").unwrap());
-
-            loop {
-                // let since = DateTime::<Utc>::from(DateTime::parse_from_rfc3339("2023-12-24T04:08:00-00:00").unwrap());
-                match request_chart_multi_data_since(tx_db3.clone(), most_recent_date).await{
-                    Ok(chart_vec) => {
-
-                        // TODO: remove; just results in a lot of unnecessary javascript work; revisit with WASM
-                        // most_recent_date = get_most_recent_date(chart_vec.clone());
-                        // tracing::debug!("[main] chart_data total: {}", &json_str);
-
-                        if let Ok(json_str) = serde_json::to_string::<Vec<ChartDataset>>(&chart_vec){
-
-                            // tracing::debug!("[main] chart in ws_broadcast thread: {}", &json_str);
-
-                            // server_tx.send(ws_broadcast::command::Cmd::Broadcast(format!("hello from test: {i}"))).unwrap();
-                            server_tx.send(ws_broadcast::command::Cmd::Broadcast(json_str)).unwrap();
-
-                        }
-
-                    },
-                    Err(_) => {}
-                }
-
-                std::thread::sleep(Duration::from_secs(1));
-            }
-
-        });
-
+        // start web server
         tracing::info!("[main] web server starting on http://127.0.0.1:8080");
         match http_server::run(tx_db2).await{
             Ok(_) => tracing::debug!("[main] web server started on http://127.0.0.1:8080"),
@@ -98,29 +65,38 @@ fn main() {
 
 }
 
+/// todo: potentially only send data since last websocket broadcast (results in annoying client side
+/// javascript so this current sends all chart data every second)
+fn spawn_chart_refresher(tx_db: Sender<DbMsg>, server_tx: Sender<Cmd>) {
+    tokio::spawn(async move {
+        // a long, long time ago...
+        let most_recent_date: DateTime<Utc> = DateTime::<Utc>::from(DateTime::parse_from_rfc3339("2023-12-24T04:08:00-00:00").unwrap());
+        loop {
+            match request_chart_multi_data_since(tx_db.clone(), most_recent_date).await{
+                Ok(chart_vec) => {
+                    if let Ok(json_str) = serde_json::to_string::<Vec<ChartDataset>>(&chart_vec){
+                        server_tx.send(Cmd::Broadcast(json_str)).unwrap();
+                    }
+                },
+                Err(_) => {}
+            }
+            std::thread::sleep(Duration::from_secs(1));
+        }
+    });
+}
 
-/// TODO: extremely inefficient copying just to get the most recent date
 fn _get_most_recent_date(chart_data: Vec<ChartDataset>) -> DateTime<Utc> {
-
-
     // todo: de-clone() all this
     let test: Vec<Vec<DateTime<Utc>>> = chart_data.iter().map(|a| a.data.iter().map(|b| b.x).collect()).collect();
     tracing::debug!("[main] chart_vec, test len: {}", &test.len());
-
     let test2: Vec<DateTime<Utc>> = test.iter().map(|x|x.clone()).flatten().collect::<Vec<DateTime<Utc>>>();
-
     tracing::debug!("[main] chart_vec, test2 len: {}", &test2.len());
-
-
     let max:DateTime<Utc> = match test2.iter().max(){
         Some(m) => m.clone(),
         None => DateTime::<Utc>::from(DateTime::parse_from_rfc3339("2023-12-01T04:08:00-00:00").unwrap())
     };
-
     tracing::debug!("[get_most_recent_date] {}", max);
-
     max
-
 }
 
 /// duplicated from visual
